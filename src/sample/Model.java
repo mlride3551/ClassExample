100b91915782d31bbd20e073d4559c7ab7a2f383
mlride3551 <mlride3551@ung.edu>
2018-08-30 11:35:50 -0400

    Adding default files into my directory

---
+++ src/sample/Model.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class Model {
+
+    public String generatePasswordHash(String passwordToHash) {
+        String generatedPasswordHash = null;
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+        }
+    }
+}
