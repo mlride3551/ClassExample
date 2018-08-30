0677b40b50f940a78944de74fc78e3bc845add86
mlride3551 <mlride3551@ung.edu>
2018-08-30 12:11:53 -0400

    n

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,7 +7,6 @@ public class Controller {
     TextField email, username, password;
 
     public void touchLogin(ActionEvent actionEvent) {
-
         Model model = new  Model();
         String hashPassword = model.generatePasswordHash(password.getText());
         System.out.print(hashPassword);
